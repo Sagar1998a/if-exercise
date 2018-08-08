ce97635f3b235bac334e21e892e1c4d7c899dad0
Sagar Devkota <30004439@student.toiohomai.ac.nz>
2018-08-08 16:45:26 +1200

    msg

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            int Num1 = 0;
+            Console.WriteLine("enter a integer");
+             Num1=int.Parse(Console.ReadLine());
+            if (Num1 % 2 == 0)  
+            {
+                Console.WriteLine("the number is even");
+            }
+                else
+            { 
+                Console.WriteLine("the number is odd");
+            }
+            Console.ReadLine();
+        
+
+
+        }
+    }
+}
